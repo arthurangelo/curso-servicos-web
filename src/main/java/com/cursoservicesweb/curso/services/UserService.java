414e41f8cf5f2db2f5f3e51508c6d7cdec91142a
Arthur Angelo de Carvalho <arthurangelo.ptc@gmail.com>
2019-08-10 16:38:02 -0300

    adicao de metodos get e post

---
+++ src/main/java/com/cursoservicesweb/curso/services/UserService.java
@@ -0,0 +1,27 @@
+package com.cursoservicesweb.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursoservicesweb.curso.entities.User;
+import com.cursoservicesweb.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
