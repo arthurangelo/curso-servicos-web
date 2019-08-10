7fc83b0a63b4b4e7f5226741fa283ca497fe590e
Arthur Angelo de Carvalho <arthurangelo.ptc@gmail.com>
2019-08-10 16:21:46 -0300

    criacao da classe de configuracao

---
+++ src/main/java/com/cursoservicesweb/curso/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.cursoservicesweb.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursoservicesweb.curso.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
