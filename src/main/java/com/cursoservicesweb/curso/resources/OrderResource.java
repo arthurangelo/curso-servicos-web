3976cd96bc5bba11e97011861cc16934fb12c41c
Arthur Angelo de Carvalho <arthur.angelo.carvalho@everis.com>
2019-10-25 17:47:19 -0300

    aula 6-9

--- src/main/java/com/cursoservicesweb/curso/resources/OrderResource.java
+++ src/main/java/com/cursoservicesweb/curso/resources/OrderResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,8 @@ public class OrderResource {
 	
 	@Autowired
 	private OrderService service;
-	
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@GetMapping
 	public ResponseEntity<List<OrderDTO>> findAll(){
 		List<OrderDTO> list = service.findAll();
@@ -27,7 +29,7 @@ public class OrderResource {
 		return ResponseEntity.ok().body(list);
 		
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<OrderDTO> findbyId(@PathVariable Long id){
 		OrderDTO obj = service.findById(id);
--- src/main/java/com/cursoservicesweb/curso/resources/ProductResource.java
+++ src/main/java/com/cursoservicesweb/curso/resources/ProductResource.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,12 +27,12 @@ public class ProductResource {
 
     @GetMapping
     public ResponseEntity<Page<ProductDTO>> findAllPaged(
-            @RequestParam(value = "page",defaultValue = "0") Integer page
-			,@RequestParam(value = "linesPerPage",defaultValue = "12") Integer linesPerpage
-			,@RequestParam(value = "orderBy",defaultValue = "name") String orderBy
-			,@RequestParam(value = "direction",defaultValue = "ASC") String direction
-	) {
-		PageRequest pageRequest = PageRequest.of(page,linesPerpage, Sort.Direction.valueOf(direction),orderBy);
+            @RequestParam(value = "page", defaultValue = "0") Integer page
+            , @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerpage
+            , @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+            , @RequestParam(value = "direction", defaultValue = "ASC") String direction
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerpage, Sort.Direction.valueOf(direction), orderBy);
         Page<ProductDTO> list = service.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
 
@@ -43,6 +44,7 @@ public class ProductResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping
     public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductCategoriesDTO obj) {
         ProductDTO newDTO = service.insert(obj);
@@ -57,6 +59,7 @@ public class ProductResource {
         return ResponseEntity.ok().body(dto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
